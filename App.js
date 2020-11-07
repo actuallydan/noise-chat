b50562997efe20a7029d7181fe88840075081e88
actuallyDan <dankral01@gmail.com>
2020-11-07 11:29:32 -0500

    fixed .env issue

--- App.js
+++ App.js
@@ -26,13 +26,13 @@ setGlobal({
 
 // init firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDlWJhtsk4HIeLU_2KeoP3IQhGufKGspMQ",
-  authDomain: "quibble-14b59.firebaseapp.com",
-  databaseURL: "https://quibble-14b59.firebaseio.com",
-  projectId: "quibble-14b59",
-  storageBucket: "quibble-14b59.appspot.com",
-  messagingSenderId: "90065254796",
-  appId: "1:90065254796:web:7987ad25399f30134b3ea9",
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
